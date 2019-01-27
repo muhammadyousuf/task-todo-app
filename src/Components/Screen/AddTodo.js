baf3f27a1689a5c3add778fc8926a8205abe977c
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-01-27 19:45:58 +0500

    firebase settiing

--- src/Components/Screen/AddTodo.js
+++ src/Components/Screen/AddTodo.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Paper, TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button'; 
-import {textBoxStyle,paperStyle, buttonStyle} from '../Style/style'
+import {textBoxStyle,paperStyle, buttonStyle} from '../Style/style';
+import * as firebase from 'firebase';
 
 const styles = theme => ({
     container: {
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../src/Components/Confiq/firebase'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 const theme = createMuiTheme();
+
 
 ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
 
