ec4b2c7d2c554a169c72e56112dd272d6e7e0cd0
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-01-27 18:37:19 +0500

    add material ui class aur libray

--- src/Components/Screen/AddTodo.js
+++ src/Components/Screen/AddTodo.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField'
 
-
+const styles = theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+  });
+  
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
+  
+  
 
 export default class AddTodo extends Component {
     render() {
+        const { classes } = this.props;
         return (
             <div>
+                <h1>Hello</h1>
             </div>
         );
     }
--- src/Components/Screen/Main.js
+++ src/Components/Screen/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AddTodo from './AddTodo';
 
 
 
@@ -6,6 +7,7 @@ export default class Main extends Component {
     render() {
         return (
             <div>
+                <AddTodo />
             </div>
         );
     }
