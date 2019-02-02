749b5a8415aab4653ec7e0861b59d7a22d4573e3
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-02 14:29:37 +0500

    retrive todo list in component did mount

--- src/Components/Screen/TodoList.js
+++ src/Components/Screen/TodoList.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import Connect from '../Function/api';
 
 
 class TodoList extends Component {
-    render(){
-        return(
+    constructor(props) {
+        super();
+        this.state = {
+            TodoList: []
+        }
+    }
+    componentDidMount() {
+        let record = {}
+        Connect(record, "get", "/")
+            .then(res => res.json())
+            .then((json) => console.log(json))
+            .catch((err) => console.log('err', err))
+    }
+    render() {
+        return (
             <div>
+
             </div>
         )
     }
