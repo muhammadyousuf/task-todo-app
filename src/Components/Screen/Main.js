b62a8f13a5104dbe2d9e2ddbc35f51053b7a9554
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-02 14:20:41 +0500

    import todolist in to main component

--- src/Components/Screen/Main.js
+++ src/Components/Screen/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AddTodo from './AddTodo';
 import NavBar from './NavBar';
+import TodoList from './TodoList';
 
 
 export default class Main extends Component {
@@ -9,6 +10,7 @@ export default class Main extends Component {
             <div>
                 <NavBar />
                 <AddTodo />
+                <TodoList />
             </div>
         );
     }
