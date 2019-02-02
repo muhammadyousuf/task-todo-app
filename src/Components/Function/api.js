5e78a59900388cc64c1f9a939c9122e0782b2848
Muhammad Yousuf <muhammadyousuf327@gmail.com>
2019-02-02 15:30:17 +0500

    add post service

--- src/Components/Function/api.js
+++ src/Components/Function/api.js
@@ -1,18 +1,32 @@
 const baseURL = 'https://todo-backend-rails.herokuapp.com';
-export default async function Connect(payload,methods, url) {
-  try {
-     if(methods === "get"){
-      let response = await fetch(
-        baseURL + `${url}`,
-        {
-          method: methods,
-        })
-      
-      return response
+export default async function Connect(payload, methods, url) {
+    try {
+        if (methods === "post") {
+            let response = await fetch(
+                baseURL + `${url}`,
+                {
+                    method: methods,
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                })
 
+            return response
+        }
+        else if (methods === "get") {
+            let response = await fetch(
+                baseURL + `${url}`,
+                {
+                    method: methods,
+                })
+
+            return response
+
+        }
     }
-  }
-  catch (error) {
-    console.error(error);
-  }
+    catch (error) {
+        console.error(error);
+    }
 }
